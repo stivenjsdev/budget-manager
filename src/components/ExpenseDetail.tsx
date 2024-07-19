89c3dcde643756573d3c6eafcf1edc2fe4042128
Sensei Stiven <strujillodev@gmail.com>
2024-07-19 16:40:06 -0500

    feat: show expenses

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,13 +1,28 @@
+import { useMemo } from "react";
+import { formatDate } from "../helpers";
 import { Expense } from "../types";
+import AmountDisplay from "./AmountDisplay";
 
 type ExpenseDetailProps = {
   expense: Expense;
-}
+};
 
 function ExpenseDetail({ expense }: ExpenseDetailProps) {
+  const formatedDate = useMemo(() => formatDate(expense.date), [expense])
   return (
-    <div>ExpenseDetail</div>
-  )
+    <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
+      <div></div>
+
+      <div>
+        <p className="text-base">{expense.expenseName}</p>
+        <p className="text-slate-600 text-sm">{formatedDate}</p>
+      </div>
+
+      <AmountDisplay
+        amount={expense.amount}
+      />
+    </div>
+  );
 }
 
-export default ExpenseDetail
+export default ExpenseDetail;
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -4,3 +4,17 @@ export function formatCurrency(amount: number) {
     currency: "COP",
   }).format(amount);
 }
+
+export function formatDate(dateStr: string): string {
+  // cambia el gtm-0500 por el correspondiente a tu zona horaria
+  console.log('formatDate run');
+  const dateObj = new Date(dateStr + " GMT-0500");
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return new Intl.DateTimeFormat("es-ES", options).format(dateObj);
+}
