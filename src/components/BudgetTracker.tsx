b0e3ffb992293893feaea29d130042796e7e48c5
Sensei Stiven <strujillodev@gmail.com>
2024-07-22 01:13:43 -0500

    feat: show budget, total spent, remaining budget

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,33 +1,40 @@
+import { useMemo } from "react";
+import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
 
 function BudgetTracker() {
-  return <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-    <div className="flex justify-center">
-      <img src="/grafico.jpg" alt="Grafica de gastos" />
-    </div>
+  const { state } = useBudget();
 
-    <div className="flex flex-col justify-center items-center gap-8">
-      <button
-        type="button"
-        className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg active:scale-95"
-      >
-        Resetear App
-      </button>
+  const totalExpenses: number = useMemo(
+    () => state.expenses.reduce((total, expense) => expense.amount + total, 0),
+    [state.expenses]
+  );
 
-      <AmountDisplay
-        label="Presupuesto"
-        amount={300}
-      />
-      <AmountDisplay
-        label="Disponible"
-        amount={200}
-      />
-      <AmountDisplay
-        label="Gastado"
-        amount={100}
-      />
+  const remainingBudget: number = useMemo(
+    () => state.budget - totalExpenses,
+    [totalExpenses, state.budget]
+  );
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+      <div className="flex justify-center">
+        <img src="/grafico.jpg" alt="Grafica de gastos" />
+      </div>
+
+      <div className="flex flex-col justify-center items-center gap-8">
+        <button
+          type="button"
+          className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg active:scale-95"
+        >
+          Resetear App
+        </button>
+
+        <AmountDisplay label="Presupuesto" amount={state.budget} />
+        <AmountDisplay label="Disponible" amount={remainingBudget} />
+        <AmountDisplay label="Gastado" amount={totalExpenses} />
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default BudgetTracker;
